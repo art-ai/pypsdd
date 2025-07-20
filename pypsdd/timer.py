8eb311d8432b6535ef91ab3551473361d58f9c81
arthur <achoi13@kennesaw.edu>
2025-07-20 16:17:29 -0400

    updated timer

--- pypsdd/timer.py
+++ pypsdd/timer.py
@@ -8,26 +8,40 @@ class Timer:
     --------
 
     >>> import time
-    >>> with Timer("timing"):
+    >>> with Timer():
     ...   time.sleep(2)
     ...
     = timing ... 2.002s
-    >>> with Timer("timing",prefix="# "):
+    >>> with Timer("hello",prefix="# "):
     ...   time.sleep(2)
     ...
-    # timing ... 2.002s
+    # hello ... 2.002s
+    >>> my_timer = Timer(verbose=False)
+    >>> with my_timer:
+    ...   time.sleep(2)
+    ...
+    >>> print("%.3fs (%d)" % (my_timer.total_time, my_timer.total_calls))
+    2.000s (1)
     """
 
-    def __init__(self,msg,prefix="= "):
+    def __init__(self,msg="timing",prefix="= ",verbose=True):
         self.msg = msg
         self.prefix = prefix
+        self.verbose = verbose
+        self.total_time = 0.0
+        self.total_calls = 0
 
     def __enter__(self):
-        print self.prefix + self.msg + " ...",
-        sys.stdout.flush()
+        if self.verbose:
+            print( self.prefix + self.msg + " ... ", end="")
+            sys.stdout.flush()
         self.start = time.time()
         return self
 
     def __exit__(self,type,value,traceback):
-        print "%.3fs" % (time.time()-self.start)
-        sys.stdout.flush()
+        elapsed = time.time()-self.start
+        self.total_time += elapsed
+        self.total_calls += 1
+        if self.verbose:
+            print( "%.3fs" % elapsed)
+            sys.stdout.flush()
